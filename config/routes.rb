becb440f3fb62c31db3415f61d0f4bd8cf366a1d
hh1997421 <haruto.10120815@gmail.com>
2020-10-14 19:02:53 +0900

    既読機能のサーバーサイド実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root to: 'posts#index'
   post 'posts', to: 'posts#create'
+  # メモのidを取得できるようにルーティング 'posts/:id'pathパラメーター
+  get 'posts/:id', to: 'posts#checked'
 end
